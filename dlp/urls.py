85667d941afe61422c6d50f8bbad4ac9fd9621fc
Alexander Putin <alik-put@yandex-team.ru>
2022-04-16 18:59:18 +0300

    fix url error

--- dlp/urls.py
+++ dlp/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('list_dir', views.list_dir, name='list_dir'),
     path('read_file/', views.read_file, name='read_file'),
     path('upload', views.upload_file, name='upload_file'),
-    path('error', views.get_error, name='error')
 ]
